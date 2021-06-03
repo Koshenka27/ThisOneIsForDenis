e379d94e2b66a037c7ba61e8ce313a0ba21f8876
Logan Koshenka <koshenka27@gmail.com>
2021-06-02 22:23:45 -0400

    Initial Commit

---
+++ ThisOneIsForDenis/ThisOneIsForDenisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThisOneIsForDenisApp.swift
+//  ThisOneIsForDenis
+//
+//  Created by Logan Koshenka on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct ThisOneIsForDenisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
